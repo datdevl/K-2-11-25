76a5985743e54067a78199058f0ca359c84bf765
datdevl <dat.dev.vl@gmail.com>
2025-12-08 13:54:27 +0700

    Update bai4.cpp

--- Lập Trình Cơ Bản/BTVN/bai4.cpp
+++ Lập Trình Cơ Bản/BTVN/bai4.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 using namespace std;
-int main() {
-    float thue=0.0675;
-    float tien=88.67;
-    float tip=0.2;
-    float s=tien+(tien*thue)+(tien*tip);
-    cout<<"Số tiền phải trả là: "<<tien<<" $"<<endl;
-    cout<<"Số tiền thuế là: "<<tien*thue<<" $"<<endl;
-    cout<<"Số tiền tip là: "<<tien*tip<<" $"<<endl;
-    cout<<"Tổng số tiền phải trả là: "<<s<<" $"<<endl;
-}
+int main(){
+	float chiphi = 88.67;
+	float thue;
+	float tip;
+	float tongtien;
+	thue = chiphi*6.75/100;
+	tip =  (chiphi+thue)*20/100;
+	tongtien   = chiphi + thue + tip;
+	cout << chiphi << endl;
+	cout << thue << endl;
+	cout << tip << endl;
+	cout << tongtien << endl;
+	
+	return 0;
+	}
